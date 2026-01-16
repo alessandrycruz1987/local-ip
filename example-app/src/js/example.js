ddd51deab8f7b4fc2a4929e6fbb94df39dc5a238
Alessandry <alessandrycruz@outlook.com>
2026-01-15 21:43:52 -0600

    Removed example-app Files

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { LocalIpPlugin } from '@cappitolian/local-ip';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    LocalIpPlugin.echo({ value: inputValue })
-}
